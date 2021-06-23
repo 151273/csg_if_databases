b309d165a4c86864a3f37b39c0889c830960f673
151273 <151273@csgaugustinus.nl>
2021-06-23 10:08:16 +0000

    .

--- public/mijnProject/index.php
+++ public/mijnProject/index.php
@@ -26,22 +26,24 @@ if (!$DBverbinding) {
 <body>
     <!-- Top Navigation Menu -->
     <div class="topnav">
-        <a href="#home" class="active"><img id= "logo" src="images/logo_small.png"></a>
+        <a href="#home" class="active"><img id="logo" src="images/logo_small.png"></a>
         <!-- Navigation links (hidden by default) -->
         <div id="myLinks">
-            <a href= "index.php">Home</a>
-             <a href="html/actie.php">Actie</a>
-             <a href="html/komedie.html">Komedie</a>
-             <a href="html/romantiek.html">Romantiek</a>
-             <a href="html/sci-fic.html">Sci-fi</a>
-             <a href="html/fantasie.html">Fantasie</a>
-             <a href="html/voorhethelegezin.html">Voor het hele gezin</a>      
+            <a href="index.php">Home</a>
+            <a href="html/actie.php">Actie</a>
+            <a href="html/komedie.html">Komedie</a>
+            <a href="html/romantiek.html">Romantiek</a>
+            <a href="html/sci-fic.html">Sci-fi</a>
+            <a href="html/fantasie.html">Fantasie</a>
+            <a href="html/voorhethelegezin.html">Voor het hele gezin</a>
         </div>
         <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
             <i class="fa fa-bars"></i>
-        </a>
-    
+</a>
+        
+
+
         <script>
             /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
             function myFunction() {
@@ -54,11 +56,105 @@ if (!$DBverbinding) {
             }
         </script>
     </div>
-    <div id="container">
-        <h1>
-            <?php echo 'een <strong>klein</strong> stukje PHP'; ?>
-        </h1>
-    </div>
+    
+        <h1> Welkom bij rate a film!</h1>
+            <?php echo 'Op deze website kun je verschillende films raten. Dit zijn films die horen bij de genres die je kan raten zijn actie films, romantische films, films voor het hele gezin, fantasie films en komedie films. '; ?>
+        
+    
 </body>
+ 
+<!-- Button to open the modal login form -->
+<button onclick="document.getElementById('id01').style.display='block'">Login</button>
+
+<!-- The Modal -->
+<div id="id01" class="modal">
+  <span onclick="document.getElementById('id01').style.display='none'"
+class="close" title="Close Modal">&times;</span>
+
+  <!-- Modal Content -->
+  <form class="modal-content animate" action="/action_page.php">
+    <div class="imgcontainer">
+      <img src="img_avatar2.png" alt="Avatar" class="avatar">
+    </div>
+
+    <div class="container">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+
+      <button type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form>
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+
+<!-- Button to open the modal -->
+<button onclick="document.getElementById('id01').style.display='block'">Sign Up</button>
+
+<!-- The Modal (contains the Sign Up form) -->
+<div id="id01" class="modal">
+  <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">times;</span>
+  <form class="modal-content" action="/action_page.php">
+    <div class="container">
+      <h1>Sign Up</h1>
+      <p>Please fill in this form to create an account.</p>
+      <hr>
+      <label for="email"><b>Email</b></label>
+      <input type="text" placeholder="Enter Email" name="email" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+
+      <label for="psw-repeat"><b>Repeat Password</b></label>
+      <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+
+      <label>
+        <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+      </label>
+
+      <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+      <div class="clearfix">
+        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+        <button type="submit" class="signup">Sign Up</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+
+<img src="images/voorkant.jpg">
 
 </html>
